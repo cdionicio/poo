e70b7d5bdbc427f3b76d1d27dab2a20c201ab649
cdionicio <160557942+cdionicio@users.noreply.github.com>
2024-02-27 23:08:37 -0600

    Agregando tarea 3

---
+++ while.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+main(){
+	/*
+	int i = 0;
+	// si la condicion es verdadera el ciclo se sigue ejecutnado
+	while(i<5){
+		 //error de novato, ciclo infinito
+		i++;
+		cout<<i<<endl;
+		
+	}
+	*/
+	/*
+	int i = 5;
+	// si la condicion es verdadera el ciclo se sigue ejecutnado
+	while(i>0){
+		 //error de novato, ciclo infinito
+		i--;
+		cout<<i<<endl;
+		
+	}
+	*/
+	/*
+
+	int i = 0;
+	do{
+	
+		i++;
+		cout<<i<<endl;
+	}while(i<5);
+	
+	
+	
+	system("pause");
+	
+}
+*/
+
+
+
+
+char respuesta;
+	do{
+		cout<<"desea ingresar otro valor(s/n)";
+		cin>>respuesta;
+		}while(respuesta== 's'||respuesta=='s');
+	
+		
+	system("pause");
+	
+}
